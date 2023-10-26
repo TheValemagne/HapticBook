cfe05fc7ed0da240927ece5b215a4e805330fac0
Baptiste Ledoyen <baptisteledoyen@gmail.com>
2023-10-26 11:42:42 +0200

    work load Label Image

--- Model/Element.cpp
+++ Model/Element.cpp
@@ -3,10 +3,12 @@
 #include <QMimeData>
 #include <QDrag>
 #include <QPixmap>
-
+#include <QDebug>
 Element::Element(const QPixmap &image, QWidget *parent):
     QLabel("Writing test")
 {
+    if(image.isNull())
+       qDebug()<<"isNull image ";
     setPixmap(image);
     move(50, 50);
     setParent(parent);
--- pageone.cpp
+++ pageone.cpp
@@ -1,13 +1,17 @@
 #include "pageone.h"
 #include "ui_pageone.h"
 #include"Model/Element.h"
-
+#include<QUrl>
+#include<QString>
+#include<QDebug>
 PageOne::PageOne(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::PageOne)
 {
     ui->setupUi(this);
-    image = QPixmap(":/resources/Images/space_ship.png"); // Remplacez par le chemin de votre image
+    image = QPixmap(":/images/Images/space_ship.png"); // Remplacez par le chemin de votre image
+    if(image.isNull())
+       qDebug()<<"isNull image pageone";
     Element *element = new Element(image, this); // Remplacez "parentWidget" par le parent approprié
 
     // Ajoutez l'objet Element à votre interface utilisateur, par exemple, à un layout ou à un widget
